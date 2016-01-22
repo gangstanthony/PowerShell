e0f336a5df65a6867cbe2b464a0447e0193c16ad
gangstanthony <gangstanthony1@gmail.com>
2016-01-22 11:56:52 -0600

    Create Save-CSVasExcel.ps1

---
+++ Save-CSVasExcel.ps1
@@ -0,0 +1,54 @@
+function Save-CSVasExcel {
+    param (
+        [string]$CSVFile = $(Throw 'No file provided.')
+    )
+    
+    # will fail if dir does not exist
+    #$CSVFile = (gi (split-path $CSVFile)).fullname + (split-path $CSVFile -leaf)
+
+    BEGIN {
+        function Resolve-FullPath ([string]$Path) {    
+            if ( -not ([System.IO.Path]::IsPathRooted($Path)) ) {
+                # $Path = Join-Path (Get-Location) $Path
+                $Path = "$PWD\$Path"
+            }
+            [IO.Path]::GetFullPath($Path)
+        }
+
+        function Release-Ref ($ref) {
+            ([System.Runtime.InteropServices.Marshal]::ReleaseComObject([System.__ComObject]$ref) -gt 0)
+            [System.GC]::Collect()
+            [System.GC]::WaitForPendingFinalizers()
+        }
+        
+        $CSVFile = Resolve-FullPath $CSVFile
+        $xl = New-Object -com 'Excel.Application'
+    }
+
+    PROCESS {
+        $wb = $xl.workbooks.open($CSVFile)
+        $xlOut = $CSVFile -replace '\.csv$', '.xlsx'
+        $ws = $wb.Worksheets.Item(1)
+        $range = $ws.UsedRange 
+        [void]$range.EntireColumn.Autofit()
+
+        $num = 1
+        $dir = Split-Path $xlOut
+        $base = $(Split-Path $xlOut -Leaf) -replace '\.xlsx$'
+        $nextname = $xlOut
+        while (Test-Path $nextname) {
+            $nextname = Join-Path $dir $($base + "-$num" + '.xlsx')
+            $num++
+        }
+
+        $wb.SaveAs($nextname, 51)
+    }
+
+    END {
+        $xl.Quit()
+    
+        $null = $ws, $wb, $xl | % {Release-Ref $_}
+
+        # del $CSVFile
+    }
+}
