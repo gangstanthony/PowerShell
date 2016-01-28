568584c43e09e1804426fe6cbcecd14abf8c01f5
gangstanthony <gangstanthony1@gmail.com>
2016-01-28 14:03:48 -0600

    Update Get-ComputerShares.ps1

--- Get-ComputerShares.ps1
+++ Get-ComputerShares.ps1
@@ -17,72 +17,66 @@ function Get-ComputerShares {
                 $n = 1,
                 [switch]$showhost
             )
-        
+
             begin {
                 $ping = new-object System.Net.NetworkInformation.Ping
             }
-        
+
             process {
                 if (!$comps) {Throw 'No host provided'}
                 foreach ($comp in $comps) {
-                    $success = $false
                     for ($i = 0; $i -lt $n; $i++) {
                         try{ $result = $ping.send($comp, 500) }catch{}
-                        $status = $result.status
-                        if ($status -eq 'Success') {
-                            $success = $true
-                        } else {
-                            $success = $false
+                        switch ($result.status) {
+                            'Success' { $success = $true }
+                            default { $success = $false }
                         }
                     }
-        
+
                     if ($showhost) {
-                        if ($success) {
-                            $result = "True  $(try{$result.address.tostring()}catch{$comp})"
-                        } else {
-                            $result = "False $comp"
+                        switch ($success) {
+                            $true { "True  $(try{ $result.address.tostring() }catch{ $comp })" }
+                            $false { "False $comp" }
                         }
                     } else {
-                        if ($success) {
-                            $result = $true
-                        } else {
-                            $result = $false
+                        switch ($success) {
+                            $true { $true }
+                            $false { $false }
                         }
                     }
-                    $result
                 }
             }
         }
     }
-    
+
     PROCESS {
         if (!$comps) {Throw 'No comps.'}
-        
-        $i = 1
+
         $total = $comps.Count
-        $starttime = Get-Date
-        $lasttime = Get-Date
+        $starttime = $lasttime = Get-Date
         foreach ($comp in $comps) {
-        
-            $currtime = $((Get-Date) - $starttime) -replace '\..*'
-            $time = $currtime.split(':')
-            $avg = (([int]$time[0] * 60 * 60) + ([int]$time[1] * 60) + [int]$time[2]) / $i
-            $last = ($((Get-Date) - $lasttime) -replace '\..*').split(':')
-            $last = ([int]$last[0] * 60 * 60) + ([int]$last[1] * 60) + [int]$last[2]
-            $left = $total - $i
-        
-            if ($total -gt 1) {
-                Write-Progress `
-                    -Activity "Get-ComputerShares $(Get-Date -f yyyy-MM-dd_HH:mm:ss) Total: $currtime Avg: $('{0:N2}' -f $avg) Last: $('{0:N0}' -f $last) ETA: $('{0:N2}' -f (($avg * $left) / 60)) min ($(((get-date).AddSeconds($avg*$left) -f '') -replace '^[^ ]+ '))" `
-                    -Status "$i of $total ($left left) [$('{0:N0}' -f (($i/$total)*100))%]" `
-                    -CurrentOperation "COMP: $comp" `
-                    -PercentComplete (($i/$total)*100)
-                $i++
-            }
-            $lasttime = Get-Date
     
+            $index++
+            $currtime = (Get-Date) - $starttime
+            $avg = $currtime.TotalSeconds / $index
+            $last = ((Get-Date) - $lasttime).TotalSeconds
+            $left = $total - $index
+            Write-Progress `
+                -Activity ((
+                    "Get-ComputerShares $(Get-Date -f yyyy-MM-dd_HH:mm:ss)",
+                    "Total: $($currtime -replace '\..*')",
+                    "Avg: $('{0:N2}' -f $avg)",
+                    "Last: $('{0:N2}' -f $last)",
+                    "ETA: $('{0:N2}' -f (($avg * $left) / 60))",
+                    "min ($([string](Get-Date).AddSeconds($avg*$left) -replace '^.* '))"
+                ) -join ' ') `
+                -Status "$index of $total ($left left) [$('{0:N2}' -f (($index/$total)*100))%]" `
+                -CurrentOperation "COMP: $comp" `
+                -PercentComplete (($index/$total)*100)
+            $lasttime = Get-Date
+
             if (ping1 $comp) {
-                
+            
                 # get ip and site name
                 $ip = ping1 $comp -showhost
                 if ($ip -match '\[') {
@@ -90,28 +84,28 @@ function Get-ComputerShares {
                 } else {
                     $ip = $ip -replace '^[^0-9]+'
                 }
-    
+
                 # THE MAIN COMMAND
                 $netview = cmd /c "net view $comp 2>&1" | ? {$_}
-    
+
                 if ($netview.count -lt 5) { continue }
-    
+
                 $netview = $netview | ? {$_  -match '  '}
                 $netview = $netview[1..($netview.count-1)]
-    
+
                 foreach ($line in $netview) {
                     $line = $line -split '  +'
                     $sharename = $line[0]
                     $type = $line[1]
                     $comment = $line[2]
-    
-                    $obj = New-Object psobject
-                    $obj | Add-Member -MemberType NoteProperty -Name IP -Value $ip
-                    $obj | Add-Member -MemberType NoteProperty -Name Computer -Value $(if ($name) {$name} else {$comp})
-                    $obj | Add-Member -MemberType NoteProperty -Name ShareName -Value $sharename
-                    $obj | Add-Member -MemberType NoteProperty -Name Type -Value $type
-                    $obj | Add-Member -MemberType NoteProperty -Name Comment -Value $comment
-                    $obj
+
+                    [pscustomobject]@{
+                        IP = $ip
+                        Computer = if ($name) {$name} else {$comp}
+                        ShareName = $sharename
+                        Type = $type
+                        Comment = $comment
+                    }
                 }
             }
         }
