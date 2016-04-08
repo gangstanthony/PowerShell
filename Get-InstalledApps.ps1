919b526861de9429d3fc5eae2bc0a5a4cfedc464
gangstanthony <gangstanthony1@gmail.com>
2016-04-08 10:08:58 -0500

    Update Get-InstalledApps.ps1

--- Get-InstalledApps.ps1
+++ Get-InstalledApps.ps1
@@ -12,12 +12,13 @@ function Get-InstalledApps {
                 $program = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine',$comp).OpenSubKey("SOFTWARE$key\Microsoft\Windows\CurrentVersion\Uninstall\$app")
                 $name = $program.GetValue('DisplayName')
                 if ($name) {
-                    New-Object PSObject -Property @{
-                        'DisplayName'     = $name
-                        'DisplayVersion'  = $program.GetValue('DisplayVersion')
-                        'Publisher'       = $program.GetValue('Publisher')
-                        'InstallDate'     = $program.GetValue('InstallDate')
-                        'UninstallString' = $program.GetValue('UninstallString')
+                    [pscustomobject]@{
+                        ComputerName = $comp
+                        DisplayName = $name
+                        DisplayVersion = $program.GetValue('DisplayVersion')
+                        Publisher = $program.GetValue('Publisher')
+                        InstallDate = $program.GetValue('InstallDate')
+                        UninstallString = $program.GetValue('UninstallString')
                     }
                 }
             }
