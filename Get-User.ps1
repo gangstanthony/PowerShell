e4850e9b7b68921158697650f5f615020adfe520
gangstanthony <gangstanthony1@gmail.com>
2016-04-19 09:13:08 -0500

    Update Get-User.ps1

--- Get-User.ps1
+++ Get-User.ps1
@@ -1,13 +1,14 @@
 function Get-User {
     param (
         $comps = $env:COMPUTERNAME,
-        [switch]$wmi = $false
+        [Parameter(ValidateSet=('process', 'dir', 'computersystem'))]
+        $method = 'process'
     )
     
     foreach ($computer in $comps) {
-        if (!$wmi) {
+        if ($method -eq 'dir') {
             Get-ChildItem \\$Computer\c$\users -Directory -Exclude '*$*' | % {dir $_.FullName ntuser.dat* -Force -ea  0} | sort LastWriteTime -Descending | select @{n='Computer';e={$Computer}}, @{n='User';e={Split-Path (Split-Path $_.FullName) -Leaf}}, LastWriteTime | ? user -notmatch '\.net'| group computer, user | % {$_.group | select -f 1}
-        } else {
+        } elseif ($method -eq 'computersystem') {
             try {
                 (gwmi win32_computerSystem -ComputerName $computer).username
             } catch [System.Exception] {
@@ -18,6 +19,10 @@ function Get-User {
                     return 'Error'
                 }
             }
+        } else {
+            $owners = @{}
+            gwmi win32_process -ComputerName $computer -Filter 'name = "explorer.exe"' | % {$owners[$_.handle] = $_.getowner().user}
+            get-process -ComputerName $computer explorer | % {$owners[$_.id.tostring()]}
         }
     }
 }
