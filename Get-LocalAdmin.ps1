1e60dd8ee88ceb8286a8da0c6ca7d22624d5e374
gangstanthony <gangstanthony1@gmail.com>
2016-04-19 10:33:30 -0500

    Create Get-LocalAdmin.ps1

---
+++ Get-LocalAdmin.ps1
@@ -0,0 +1,26 @@
+function Get-LocalAdmin {
+    Param (
+        $comp = $env:COMPUTERNAME,
+        [ValidateSet('adsi', 'wmi')]
+        $method = 'adsi'
+    )
+
+    if ($method -eq 'wmi') { # takes crazy long because it lists every group (even many domain groups) then finds the Administrators group
+        Get-WmiObject -Query 'SELECT GroupComponent, PartComponent FROM Win32_GroupUser' -ComputerName $comp | ? GroupComponent -Like '*"Administrators"' | % {
+            [pscustomobject]@{
+                Computer = $_.partcomponent -replace '^\\\\([^\\]+).*', '$1'
+                User = $_.partcomponent –replace '.+Domain\=(.+)\,Name\=(.+)$', '$1\$2' -replace '"'
+            }
+        }
+    } else { # adsi
+        $group = [ADSI]"WinNT://$comp/Administrators"
+        $group.Invoke('Members') | % {
+            [pscustomobject]@{
+                Computer = $comp
+                #User = $_.GetType().InvokeMember('Name', 'GetProperty', $null, $_, $null) # gives error
+                #User = $_.GetType.Invoke().InvokeMember('Name', 'GetProperty', $null, $_, $null) # works, but other way is shorter
+                User = ([ADSI]$_).InvokeGet('Name')
+            }
+        }
+    }
+}
