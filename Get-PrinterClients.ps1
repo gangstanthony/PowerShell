cb7c814cdb5e326b34415674e8a1b632394cd7c9
gangstanthony <gangstanthony1@gmail.com>
2016-04-20 11:19:23 -0500

    Create Get-PrinterClients.ps1

---
+++ Get-PrinterClients.ps1
@@ -0,0 +1,57 @@
+# KNOWN ISSUES:
+# only finds printers that are *NOT* locally hosted
+# if looking for all possible printers, might have to use Get-PrinterHosts as well
+# 
+# REQUIRES:
+# admin rights
+
+function Get-PrinterClients {
+    Param (
+        [string]$comp = $env:COMPUTERNAME
+    )
+    
+    if (!$comp) { throw 'No comp.' }
+
+    $ping = New-Object System.Net.NetworkInformation.Ping
+    try {
+        $result = $ping.Send($comp)
+    } catch {
+        $result = $null
+    }
+
+    if ($result.Status -eq 'Success') {
+        $reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('Users', $comp)
+        [string[]]$users = $reg.GetSubKeyNames() | ? {$_ -match '-\d{5}$'}
+        foreach ($user in $users) {
+            try {
+                $printers = $reg.OpenSubKey("$user\printers\connections").GetSubKeyNames()
+            } catch {
+                $printers = '-'
+            }
+
+            if ($printers -ne '-') {
+                $printers = @($printers | % {$_.Substring(2).Replace(',', '\').ToUpper()} | select -Unique) -join '; '
+            }
+            
+            $remreg = [pscustomobject]@{
+                User = ([System.Security.Principal.SecurityIdentifier]($user)).Translate([System.Security.Principal.NTAccount]).Value
+                Printer = $printers
+            }
+
+            [pscustomobject]@{
+                Computer = $comp
+                IP = $result.Address.ToString()
+                User = $remreg.user
+                Printer = $remreg.printer
+            }
+        }
+    } else {
+        [pscustomobject]@{
+            Computer = $comp
+            IP = '-'
+            User = '-'
+            Printer = '-'
+        }
+    }
+}
+
