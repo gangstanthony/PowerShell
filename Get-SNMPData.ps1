9357ea5e13cde57146b7375fb0d5247920a1bd63
gangstanthony <gangstanthony1@gmail.com>
2016-04-20 11:44:49 -0500

    Create Get-SNMPData.ps1

---
+++ Get-SNMPData.ps1
@@ -0,0 +1,41 @@
+
+# get snmp description and name
+
+function Get-SNMPData ($device) {
+
+    begin {
+        $snmp = New-Object -ComObject olePrn.OleSNMP
+    }
+
+    process {
+        $name = $desc = $addr = $ip = '-'
+
+        $ping = New-Object System.Net.NetworkInformation.Ping
+        try {
+            $result = $ping.Send($device)
+        } catch {
+            $result = $null
+        }
+
+    
+        if ($result.Status -eq 'Success') {
+            $ip = $result.Address.ToString()
+
+            $snmp.open($ip, 'public', 2, 3000)
+
+            try {
+                $name = $snmp.Get('.1.3.6.1.2.1.1.5.0')
+                $desc = $snmp.Get('.1.3.6.1.2.1.1.1.0')
+                $addr = ($snmp.Gettree('.1.3.6.1.2.1.4.20.1.1') | ? {$_ -match '(?:[^\.]{1,3}\.){3}[^\.]{1,3}$' -and $_ -notmatch '127\.0\.0\.1'} | % {$i = $_.split('.'); "$($i[-4]).$($i[-3]).$($i[-2]).$($i[-1])"}) -join ';'
+            } catch {}
+        }
+
+        [pscustomobject]@{
+            Device = $device
+            IP = $ip
+            SNMPName = $name
+            SNMPDescription = $desc
+            SNMPAddresses = $addr
+        }
+    }
+}
