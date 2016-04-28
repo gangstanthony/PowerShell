fb3daf37b5846baae8b66e89f819b090ec6eb3a2
gangstanthony <gangstanthony1@gmail.com>
2016-04-28 08:10:31 -0500

    Create fastest foreach.ps1

---
+++ fastest foreach.ps1
@@ -0,0 +1,57 @@
+$a1 = New-Object System.Collections.ArrayList
+$a2 = New-Object System.Collections.ArrayList
+$a3 = New-Object System.Collections.ArrayList
+$a4 = New-Object System.Collections.ArrayList
+$a5 = New-Object System.Collections.ArrayList
+$a6 = New-Object System.Collections.ArrayList
+
+1..100 | % {
+    [void]$a1.add((Measure-Command {
+        $b = ps
+        $b.processname
+    }).ticks)
+
+    [void]$a2.add((Measure-Command {
+        $b = ps
+        foreach ($p in $b) {$p.ProcessName}
+    }).ticks)
+    
+    [void]$a3.add((Measure-Command {
+        $b = ps
+        @($b).foreach{$_.processname}
+    }).ticks)
+
+    [void]$a4.add((Measure-Command {
+        $b = ps
+        $b | % {$_.processname}
+    }).ticks)
+
+    [void]$a5.add((Measure-Command {
+        $b = ps
+        $b | select -ExpandProperty processname
+    }).ticks)
+    
+    [void]$a6.add((Measure-Command {
+        $b = ps
+        $b | % processname
+    }).ticks)
+}
+
+''
+'$b.processname'
+$a1 | measure -Sum | % {$_.sum}
+''
+'foreach ($p in $b) {$p.ProcessName}'
+$a2 | measure -Sum | % {$_.sum}
+''
+'@($b).foreach{$_.processname}'
+$a3 | measure -Sum | % {$_.sum}
+''
+'$b | % {$_.processname}'
+$a4 | measure -Sum | % {$_.sum}
+''
+'$b | select -ExpandProperty processname'
+$a5 | measure -Sum | % {$_.sum}
+''
+'$b | % processname'
+$a6 | measure -Sum | % {$_.sum}
