443c3f2b20b40fc2f765fa637624bfce607fc607
gangstanthony <gangstanthony1@gmail.com>
2016-05-05 14:00:21 -0500

    Update touch.ps1

--- touch.ps1
+++ touch.ps1
@@ -7,9 +7,14 @@ function touch {
         throw "No filename supplied"
     }
 
+    $dir = Split-Path $file
+
     if (Test-Path $file) {
         (Get-ChildItem $file).LastWriteTime = Get-Date
+    } elseif (Test-Path $dir) {
+        echo $null > $file
     } else {
+        md $dir | Out-Null
         echo $null > $file
     }
 }
