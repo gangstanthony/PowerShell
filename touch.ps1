4ed57321d907c876b20a05c392f880f3efa69eeb
gangstanthony <gangstanthony1@gmail.com>
2016-05-05 14:14:09 -0500

    Update touch.ps1

--- touch.ps1
+++ touch.ps1
@@ -1,8 +1,6 @@
 # http://superuser.com/questions/502374/equivalent-of-linux-touch-to-create-an-empty-file-with-powershell
 
-function touch {
-    $file = $args[0]
-
+function touch ([string]$file) {
     if ($file -eq $null) {
         throw "No filename supplied"
     }
