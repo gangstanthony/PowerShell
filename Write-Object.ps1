6bd2a3f4d54e697bdfd1adbc367bb3d4773f00c3
gangstanthony <gangstanthony1@gmail.com>
2016-05-05 16:19:29 -0500

    Create Write-Object.ps1

---
+++ Write-Object.ps1
@@ -0,0 +1,30 @@
+# https://social.technet.microsoft.com/Forums/windowsserver/en-US/30663446-4091-4a1c-9de0-407046ccc39f/powershell-script-with-submenus-how-to-go-back?forum=winserverpowershell
+
+# the title is not very descriptive, but it's basically write-host $($a | fl * | out-string)
+
+function Write-Object {
+    Param (
+        [Parameter(
+            ValueFromPipeline=$True,
+            ValueFromPipelinebyPropertyName=$True)]
+        [object[]]$Object,
+        [string]$Header,
+        [string]$Footer
+    )
+    
+    if ($input) {
+        $Object = @($input)
+    }
+    
+    if (!$Object) { Throw 'Must provide an object.' }
+    
+    if ($Header) {
+        Write-Host $Header
+    }
+
+    ($Object | Format-List * | Out-String) -split "`n" | ? {$_ -notmatch '^\s+?$'} | % {Write-Host $_}
+
+    if ($Footer) {
+        Write-Host $Footer
+    }
+}
