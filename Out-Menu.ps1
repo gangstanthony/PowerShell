b30315cc53a97399d5aa2bb9faa8f318fe4bb5e2
gangstanthony <gangstanthony1@gmail.com>
2016-05-10 19:42:19 -0500

    Update Out-Menu.ps1

--- Out-Menu.ps1
+++ Out-Menu.ps1
@@ -2,6 +2,22 @@
 # https://www.reddit.com/r/PowerShell/comments/49tqgx/need_some_help_with_an_automation_script_im/
 # http://www.powertheshell.com/input_psv3/
 
+# EXAMPLE1
+# $folder = Get-ChildItem | Out-Menu
+# lists folder contents and prompts user to select one of the items
+
+# EXAMPLE 2
+# $folder = Out-Menu -Object $(Get-ChildItem) -Header '---Folder List---' -Footer '---Enter a folder number---'
+# lists folder contents between header and footer text
+
+# EXAMPLE 3
+# menu $(dir) -AllowMultiple
+# lists folder contents and allows user to select multiple items by giving a comma separated list of items (1, 2, 5)
+
+# EXAMPLE 4
+# [IO.DriveInfo]::GetDrives() | menu -AllowCancel
+# lists drives to choose from. if 'c' is entered, the menu selection is canceled and $null is returned
+
 function Out-Menu {
     param (
         [Parameter(
