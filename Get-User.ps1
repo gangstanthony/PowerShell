5733cfeeb769434556c5b2eeac724939baf0be8e
gangstanthony <gangstanthony1@gmail.com>
2016-05-17 11:27:40 -0500

    Update Get-User.ps1

--- Get-User.ps1
+++ Get-User.ps1
@@ -1,3 +1,31 @@
+## INFO
+# find last logged on user of remote computer
+
+# https://www.reddit.com/r/PowerShell/comments/448gdx/noob_need_help_getting_a_list_with_all_xp/
+
+# get-ciminstance is the successor, but it doesn't work for me...
+# http://powershell.com/cs/forums/t/23349.aspx
+
+# (gp "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI").lastloggedonuser
+# reg query \\$computer\HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI /v LastLoggedOnUser
+# [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine',$computer).OpenSubKey('SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI').getvalue('LastLoggedOnUser')
+
+# (gwmi win32_loggedonuser).antecedent # this is remote connections, not necessarily logons. includes computers and users connected to print servers
+
+<# all these give about the same information. the first two might give more
+$server = 'localhost'
+
+# IDENTICAL
+query session /server:$server
+qwinsta /server:$server
+
+# IDENTICAL
+query user /server:$server
+quser /server:$server
+
+qprocess explorer.exe /server:$server
+#>
+
 function Get-User {
     param (
         $comps = $env:COMPUTERNAME,
