3e62d14f25ec0a417073204d3652cf929a2cb4a0
gangstanthony <gangstanthony1@gmail.com>
2016-06-22 09:15:49 -0500

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -22,9 +22,13 @@ Update-TypeData "$PSScriptRoot\My.Types.Ps1xml"
 
 # http://get-powershell.com/post/2008/06/25/Stuffing-the-output-of-the-last-command-into-an-automatic-variable.aspx
 function Out-Default {
-    try {
-        $input | Tee-Object -Variable global:lastobject | Microsoft.PowerShell.Core\Out-Default
-    } catch {
+    if ($input.GetType().ToString() -ne 'System.Management.Automation.ErrorRecord') {
+        try {
+            $input | Tee-Object -Variable global:lastobject | Microsoft.PowerShell.Core\Out-Default
+        } catch {
+            $input | Microsoft.PowerShell.Core\Out-Default
+        }
+    } else {
         $input | Microsoft.PowerShell.Core\Out-Default
     }
 }
