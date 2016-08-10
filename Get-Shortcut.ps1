26f9bc8358047d39fcc48fc2a4f013f7be5db299
gangstanthony <gangstanthony1@gmail.com>
2016-08-10 12:28:43 -0500

    Create Get-Shortcut.ps1

---
+++ Get-Shortcut.ps1
@@ -0,0 +1,38 @@
+# Get-Shortcut
+# 
+# Arguments
+# Description
+# FullName
+# Hotkey
+# IconLocation = '%SystemRoot%\system32\SHELL32.dll,16' # printer
+# RelativePath
+# TargetPath
+# WindowStyle
+# WorkingDirectory
+
+function Get-Shortcut {
+    param (
+        [parameter(mandatory=$true)]
+        [string]$Path
+    )
+
+    begin {
+        $WshShell = New-Object -ComObject WScript.Shell
+    }
+
+    process {
+        if (!$Path) {Throw 'No Source'}
+
+        $Shortcut = $WshShell.CreateShortcut($Path)
+        Write-Output $Shortcut
+    }
+
+    end {
+        function Release-Ref ($ref) {
+            ([System.Runtime.InteropServices.Marshal]::ReleaseComObject([System.__ComObject]$ref) -gt 0)
+            [System.GC]::Collect()
+            [System.GC]::WaitForPendingFinalizers()
+        }
+        $Shortcut, $WshShell | % {$null = Release-Ref $_}
+    }
+}
