b4136257bd15ab5af069e4643674ded2fc58094d
gangstanthony <gangstanthony1@gmail.com>
2017-01-24 11:55:14 -0600

    Create Get-ChromeExtension.ps1

---
+++ Get-ChromeExtension.ps1
@@ -0,0 +1,14 @@
+# https://www.reddit.com/r/PowerShell/comments/5px71w/getting_chrome_extensions/
+
+function Get-ChromeExtension {
+    param (
+        [string]$ComputerName = $env:COMPUTERNAME
+    )
+
+    #$users = Get-ChildItem "\\$ComputerName\c$\users" -Directory -Name
+
+    Get-ChildItem "\\$ComputerName\c$\users\*\appdata\local\Google\Chrome\User Data\Default\Extensions\*\*\manifest.json" -ErrorAction SilentlyContinue | % {
+        $_.FullName -match 'users\\(.*?)\\appdata' | Out-Null
+        Get-Content $_.FullName | ConvertFrom-Json | select @{n='ComputerName';e={$ComputerName}}, @{n='User';e={$Matches[1]}}, name, version
+    }
+}
