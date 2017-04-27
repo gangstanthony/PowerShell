f8bc765d20b121154257a0afd495e2369987a54b
gangstanthony <gangstanthony1@gmail.com>
2017-04-27 15:11:39 -0500

    Update Get-MappedDrives.ps1

--- Get-MappedDrives.ps1
+++ Get-MappedDrives.ps1
@@ -3,13 +3,16 @@ function Get-MappedDrives {
         $computer = $env:COMPUTERNAME
     )
 
-    Get-RemoteRegistry -hive 'users' -comps $computer | ? {$_.RegKeyName -match '\d{4,5}$'} | % {
-        $sid = $_.regkeyname
-        Get-RemoteRegistry -hive Users -keys "$sid\Network" -comps $computer |
-          select `
-            @{n='Computer';e={$computer}},
-            @{n='User';e={([System.Security.Principal.SecurityIdentifier]($sid)).Translate([System.Security.Principal.NTAccount]).Value}},
-            @{n='DriveLetter';e={$_.RegKeyName}},
-            @{n='Map';e={$_.RemotePath.Value}}
+    $reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('Users', $computer)
+    $reg.GetSubKeyNames() | ? {$_ -match '\d{4,5}$'} | % {
+        $sid = $_
+        $reg.OpenSubKey("$sid\Network").GetSubKeyNames() | % {
+            [pscustomobject]@{
+                Computer = $computer
+                User = ([System.Security.Principal.SecurityIdentifier]($sid)).Translate([System.Security.Principal.NTAccount]).Value
+                DriveLetter = $_
+                Map = $reg.OpenSubKey("$sid\Network\$_").GetValue('RemotePath')
+            }
+        }
     }
 }
