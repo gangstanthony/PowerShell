b0d2e433ef9e4b1989b3f0a45ee4d5adccb6d487
gangstanthony <gangstanthony1@gmail.com>
2017-06-07 12:48:18 -0500

    Create Create-LocalUser.ps1

---
+++ Create-LocalUser.ps1
@@ -0,0 +1,40 @@
+function Create-LocalUser {
+    param (
+        [string]$computer = $env:computername,
+        [string]$fullname,
+        [string]$username,
+        [string]$password,
+        [switch]$passworddoesnotexpire,
+        [string]$addtogroup = 'Administrators',
+        [switch]$CheckFirst = $true
+    )
+
+    if (!$username -or !$password) {
+        throw 'no username or password'
+    }
+
+    if ($checkfirst -and ([ADSI]"WinNT://$computer/$username").Name) {
+        Write-Warning "$username already exists on $computer"
+        return
+    }
+
+    $objOU = [ADSI]"WinNT://$computer"
+    $objUser = $objOU.Create('user', $username)
+    $objUser.SetPassword($password)
+    $objUser.SetInfo()
+    $objUser.FullName = $fullname
+    $objUser.SetInfo()
+    #$objUser.Description = 'Test user'
+    #$objUser.SetInfo()
+
+    if ($passworddoesnotexpire) {
+        $objUser.UserFlags = 65536 # password does not expire
+        $objUser.SetInfo()
+    }
+
+    if ($addtogroup) {
+        ([ADSI]"WinNT://$computer/$addtogroup").Add("WinNT://$computer/$username")
+    }
+}
+
+# Create-LocalUser -computer $env:COMPUTERNAME -fullname 'Test' -username 'test' -password 'Password1' -passworddoesnotexpire -addtogroup 'Administrators'
