f022d689ddb1ac24dc08c84a49bad83a65bac4b3
gangstanthony <gangstanthony1@gmail.com>
2018-01-25 14:37:52 -0600

    Update set wallpaper from reddit.ps1

--- set wallpaper from reddit.ps1
+++ set wallpaper from reddit.ps1
@@ -26,60 +26,60 @@ function Set-Wallpaper {
     begin {
         try {
             Add-Type @"
-            using System;
-            using System.Runtime.InteropServices;
-            using Microsoft.Win32;
-            namespace Wallpaper
-            {
-	            public enum Style : int
-	            {
-		            Tile, Center, Stretch, Fill, Fit, Span, NoChange
-	            }
-	
-	            public class Setter
+                using System;
+                using System.Runtime.InteropServices;
+                using Microsoft.Win32;
+                namespace Wallpaper
                 {
-		            public const int SetDesktopWallpaper = 20;
-		            public const int UpdateIniFile = 0x01;
-		            public const int SendWinIniChange = 0x02;
-		            [DllImport( "user32.dll", SetLastError = true, CharSet = CharSet.Auto )]
-		            private static extern int SystemParametersInfo ( int uAction, int uParam, string lpvParam, int fuWinIni );
-		            public static void SetWallpaper ( string path, Wallpaper.Style style )
+	                public enum Style : int
+	                {
+		                Tile, Center, Stretch, Fill, Fit, Span, NoChange
+	                }
+	
+	                public class Setter
                     {
-			            SystemParametersInfo( SetDesktopWallpaper, 0, path, UpdateIniFile | SendWinIniChange );
-			            RegistryKey key = Registry.CurrentUser.OpenSubKey( "Control Panel\\Desktop", true );
-			            switch( style )
-			            {
-				            case Style.Tile :
-				            key.SetValue( @"WallpaperStyle", "0" ) ;
-				            key.SetValue( @"TileWallpaper", "1" ) ;
-				            break;
-				            case Style.Center :
-				            key.SetValue( @"WallpaperStyle", "0" ) ;
-				            key.SetValue( @"TileWallpaper", "0" ) ;
-				            break;
-				            case Style.Stretch :
-				            key.SetValue( @"WallpaperStyle", "2" ) ;
-				            key.SetValue( @"TileWallpaper", "0" ) ;
-				            break;
-				            case Style.Fill :
-				            key.SetValue( @"WallpaperStyle", "10" ) ;
-				            key.SetValue( @"TileWallpaper", "0" ) ;
-				            break;
-				            case Style.Fit :
-				            key.SetValue( @"WallpaperStyle", "6" ) ;
-				            key.SetValue( @"TileWallpaper", "0" ) ;
-				            break;
-				            case Style.Span :
-				            key.SetValue( @"WallpaperStyle", "22" ) ;
-				            key.SetValue( @"TileWallpaper", "0" ) ;
-				            break;
-				            case Style.NoChange :
-				            break;
-			            }
-			            key.Close();
-		            }
-	            }
-            }
+		                public const int SetDesktopWallpaper = 20;
+		                public const int UpdateIniFile = 0x01;
+		                public const int SendWinIniChange = 0x02;
+		                [DllImport( "user32.dll", SetLastError = true, CharSet = CharSet.Auto )]
+		                private static extern int SystemParametersInfo ( int uAction, int uParam, string lpvParam, int fuWinIni );
+		                public static void SetWallpaper ( string path, Wallpaper.Style style )
+                        {
+			                SystemParametersInfo( SetDesktopWallpaper, 0, path, UpdateIniFile | SendWinIniChange );
+			                RegistryKey key = Registry.CurrentUser.OpenSubKey( "Control Panel\\Desktop", true );
+			                switch( style )
+			                {
+				                case Style.Tile :
+				                key.SetValue( @"WallpaperStyle", "0" ) ;
+				                key.SetValue( @"TileWallpaper", "1" ) ;
+				                break;
+				                case Style.Center :
+				                key.SetValue( @"WallpaperStyle", "0" ) ;
+				                key.SetValue( @"TileWallpaper", "0" ) ;
+				                break;
+				                case Style.Stretch :
+				                key.SetValue( @"WallpaperStyle", "2" ) ;
+				                key.SetValue( @"TileWallpaper", "0" ) ;
+				                break;
+				                case Style.Fill :
+				                key.SetValue( @"WallpaperStyle", "10" ) ;
+				                key.SetValue( @"TileWallpaper", "0" ) ;
+				                break;
+				                case Style.Fit :
+				                key.SetValue( @"WallpaperStyle", "6" ) ;
+				                key.SetValue( @"TileWallpaper", "0" ) ;
+				                break;
+				                case Style.Span :
+				                key.SetValue( @"WallpaperStyle", "22" ) ;
+				                key.SetValue( @"TileWallpaper", "0" ) ;
+				                break;
+				                case Style.NoChange :
+				                break;
+			                }
+			                key.Close();
+		                }
+	                }
+                }
 "@
         } catch {}
     
