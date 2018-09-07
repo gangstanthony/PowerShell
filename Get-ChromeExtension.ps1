d3a799baf98f5b3daaef133232453112d4a2c181
gangstanthony <gangstanthony1@gmail.com>
2018-09-07 15:46:30 -0500

    Update Get-ChromeExtension.ps1

--- Get-ChromeExtension.ps1
+++ Get-ChromeExtension.ps1
@@ -6,7 +6,8 @@ function Get-ChromeExtension {
     )
 
     Get-ChildItem "\\$ComputerName\c$\users\*\appdata\local\Google\Chrome\User Data\Default\Extensions\*\*\manifest.json" -ErrorAction SilentlyContinue | % {
+        $path = $_.FullName
         $_.FullName -match 'users\\(.*?)\\appdata' | Out-Null
-        Get-Content $_.FullName -Raw | ConvertFrom-Json | select @{n='ComputerName';e={$ComputerName}}, @{n='User';e={$Matches[1]}}, name, version
+        Get-Content $_.FullName -Raw | ConvertFrom-Json | select @{n='ComputerName';e={$ComputerName}}, @{n='User';e={$Matches[1]}}, Name, Version, @{n='Path';e={$path}}
     }
 }
