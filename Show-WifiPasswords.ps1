a0424b21d87dece0dec18874e4f6deab6b02139a
gangstanthony <gangstanthony1@gmail.com>
2022-03-26 00:02:34 -0500

    Update Show-WifiPasswords.ps1

--- Show-WifiPasswords.ps1
+++ Show-WifiPasswords.ps1
@@ -2,8 +2,22 @@
 # http://www.thewindowsclub.com/delete-wifi-network-profile-windows
 # netsh wlan delete profile name="MyWifiNetwork"
 
+# one-liner
+# netsh wlan show profiles | ? {$_ -match ' : '} | % {$_.split(':')[1].trim()} | % {$n = $_; netsh wlan show profile name="$_" key=clear} | ? {$_ -match 'key content'} | select @{n='Network';e={$n}}, @{n='Key';e={$_.split(':')[1].trim()}}
+
+# https://www.reddit.com/r/PowerShell/comments/tnzyxw/showwifipasswords/
+# optimized code credit to /u/Thotaz and /u/nascentt
 function Show-WifiPasswords {
-    netsh wlan show profiles | ? {$_ -match ' : '} | % {$_.split(':')[1].trim()} | % {$n = $_; netsh wlan show profile name="$_" key=clear} | ? {$_ -match 'key content'} | select @{n='Network';e={$n}}, @{n='Key';e={$_.split(':')[1].trim()}}
+    netsh.exe wlan show profiles | Select-String -Pattern "(?<=^.+: ).+$" | ForEach-Object -Process {
+        $NetworkName = $_.Matches[0].Value
+        $KeyMatches = netsh.exe wlan show profile name="$NetworkName" key=clear | Select-String -Pattern "(?<=^\s+Key Content\s+: ).+$"
+        if ($KeyMatches) {
+            [pscustomobject]@{
+                NetworkName = $NetworkName
+                SecurityKey = $KeyMatches.Matches[0].Value
+            }
+        }
+    }
 }
 
 <# sample output
